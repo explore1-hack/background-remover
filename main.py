4c5e3124532b3916c1cf8b61a71192d611dea0ba
Ashutosh pandey <pandey.ashutosh221122@gmail.com>
2026-06-01 12:03:05 +0530

    add rate limiting and app key auth

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI , UploadFile , File ,HTTPException
+from fastapi import FastAPI , UploadFile , File ,HTTPException,Request
 from fastapi.responses import StreamingResponse
 from rembg import remove
 from PIL import Image
 import io
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi .errors import RateLimitExceeded
+
+API_KEY = "mysecretkey123"
 
 
 
 app = FastAPI()
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
 
 @app.get("/")
 def health_check():
@@ -14,10 +22,16 @@ def health_check():
 
 
 @app.post("/remove-background/")
+@limiter.limit("5/minutes")
 
-async def remove_background(file: UploadFile = File(...)):
-    if file.content_type not in ["image/jpeg", "image/png"]:
-        raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
+async def remove_background(request:Request ,file: UploadFile = File(...)):
+    api_key = request.headers.get("x-api-key")
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="invalid api key.")
+
+          
+        if file.content_type not in ["image/jpeg", "image/png"]:
+            raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
     
     try:
         input_image = await file.read()
