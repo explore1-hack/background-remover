d4999d288ff0d92ef9f7472db08331fa7050e09a
Ashutosh pandey <pandey.ashutosh221122@gmail.com>
2026-06-01 13:14:31 +0530

    testing something

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import os
 from fastapi.security import APIKeyHeader
 
 
+
+from fastapi.security import APIKeyHeader
+
+api_key_header = APIKeyHeader(name="x-api-key")  # this creates the lock button
+
+def verify_key(api_key: str = Security(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key.")
+
+
 API_KEY = os.getenv("API_KEY")
 if not API_KEY:
     raise RuntimeError("API_KEY environment variable is not set")
